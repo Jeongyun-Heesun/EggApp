be4aed707c7ec79f45a3a01b5b29174825857496
YuJungYoon <oon06173@naver.com>
2020-08-09 12:01:03 +0900

    Ready Page UI

--- app/src/main/java/com/example/eggapplication/Ready.java
+++ app/src/main/java/com/example/eggapplication/Ready.java
@@ -1,4 +1,26 @@
 package com.example.eggapplication;
 
-public class Ready {
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eggapplication.databinding.ActivityReadyBinding;
+
+public class Ready extends AppCompatActivity {
+    private ActivityReadyBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityReadyBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
+
+        for(int i=0;i<Egg.READY.length;i++){
+            binding.textView.setText(Egg.READY[i]);
+        }
+
+    }
+
 }
